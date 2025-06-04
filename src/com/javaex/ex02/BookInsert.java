a36bc9e559b0413fb76f1ec8492e03277ab09617
ahah2025 <adghty61@naver.com>
2025-06-04 17:40:23 +0900

    2025-06-04

--- src/com/javaex/ex02/BookInsert.java
+++ src/com/javaex/ex02/BookInsert.java
@@ -1,4 +1,4 @@
-package com.javaex.ex01;
+package com.javaex.ex02;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,56 +6,51 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AuthorInsert {
+public class BookInsert {
 
 	public static void main(String[] args) {
-		// 작가 데이터 저장
-
+		// book 데이터 저장
 		// 0. import java.sql.*;
-		Connection conn = null;				//주소:	conn
-		PreparedStatement pstmt = null;		//주소:	pstmt
-		//ResultSet rs = null;				//주소:	rs
-
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		try {
 			// 1. JDBC 드라이버 (MySQL) 로딩
 			Class.forName("com.mysql.cj.jdbc.Driver");
-					
+			
 			// 2. Connection 얻어오기
 			String url = "jdbc:mysql://localhost:3306/web_db";
 			conn = DriverManager.getConnection(url, "web", "web");
 			
-			// 3. SQL문 준비 / 바인딩 / 실행 - insert문
-			// sql문 준비
-			
+			// 3. SQL문 준비 / 바인딩 / 실행
+			//SQL문 준비
 			String query = "";
-			query += " insert into author ";
-			query += " values(null,?,?) "; 
+			query += " alter table book ";
+			query += " add pubs varchar(50) "; 
 			System.out.println(query);
 			
 			//바인딩(자리 찾아줌)
 			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1,"이효리");
-			pstmt.setString(2,"제주도");
+			pstmt.setString(1, "우리들의 일그러진 영웅");
+			pstmt.setString(2, "삼국지");
+			pstmt.setString(1, query);
 			
 			//실행
 			int count = pstmt.executeUpdate();
 			
 			// 4.결과처리
-			System.out.println(count+"건이 등록 되었습니다.");
+			System.out.println(count+"건이 등록되었습니다.");
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
-
 			// 5. 자원정리
 			try {
-				/*
 				if (rs != null) {
 					rs.close();
 				}
-				*/
 				if (pstmt != null) {
 					pstmt.close();
 				}
