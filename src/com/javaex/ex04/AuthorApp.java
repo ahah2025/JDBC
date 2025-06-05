598ce1c324031c81d2626153f0bab76c63733f3f
ahah2025 <adghty61@naver.com>
2025-06-05 14:53:48 +0900

    2025-06-05

---
+++ src/com/javaex/ex04/AuthorApp.java
@@ -0,0 +1,20 @@
+package com.javaex.ex04;
+
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		
+		AuthorDAO authorDAO = new AuthorDAO();
+		
+		//int c01 = authorDAO.authorInsert("김종국", "런닝맨1기");
+	
+		int c02 = authorDAO.authorUpdate(16,"이효리","제주도");
+	
+		//int c03 = authorDAO.authorDelete(2);
+		
+		//authorDAO.authorSelect();
+	
+		//List<AuthorVO> authorList = authorDAO.authorSelect();
+	
+	}
+}
