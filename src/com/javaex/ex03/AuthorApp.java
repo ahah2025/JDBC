863a80031beba32ad298d72dc8d2894730c7af52
ahah2025 <adghty61@naver.com>
2025-06-11 18:09:51 +0900

    2025

--- src/com/javaex/ex03/AuthorApp.java
+++ src/com/javaex/ex03/AuthorApp.java
@@ -7,14 +7,14 @@ public class AuthorApp {
 		
 		AuthorDAO authorDao = new AuthorDAO();
 		
-		/*
+		
 		authorDao.authorInsert("박경리", "토지작가");
 		authorDao.authorInsert("김영하", "알쓸신잡 출연");
 		authorDao.authorInsert("기안84", "방송대상 수장");
-		*/
 		
 		
-		authorDao.authorDelete(3);
+		
+		//authorDao.authorDelete(3);
 		
 		//authorDao.authorUpdate(3, "강호동", "운동선수");
 		
